548bfc7384b3845c2371d16fff749be2a302bc15
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-19 16:03:29 +0200

    Initial commit

---
+++ src/main/java/fi/skd/buttongame/ButtongameApplication.java
@@ -0,0 +1,13 @@
+package fi.skd.buttongame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButtongameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButtongameApplication.class, args);
+	}
+
+}
