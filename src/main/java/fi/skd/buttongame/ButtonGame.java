01c1125103961de45a8b013224c55dc2feffd02f
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-20 02:19:47 +0200

    Work on saving the counter.

--- src/main/java/fi/skd/buttongame/ButtonGame.java
+++ src/main/java/fi/skd/buttongame/ButtonGame.java
@@ -2,11 +2,14 @@ package fi.skd.buttongame;
 
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 @Service
 public class ButtonGame {
-
-    private int counter;
-
     public GameState play() {
         GameState state = new GameState();
         int value = runCounter();
@@ -33,7 +36,32 @@ public class ButtonGame {
     }
 
     private synchronized int runCounter() {
-        counter++;
+        int counter = 0;
+        String filename = "counter.txt";
+        File counterFile = new File(filename);
+        int ch;
+        if (!counterFile.exists()) {
+            try {
+                counterFile.createNewFile();
+                FileWriter fw = new FileWriter(counterFile);
+                counter = 1;
+                fw.write(counter);
+                fw.close();
+            } catch (IOException e) {
+                System.out.println("Something went wrong with creating the counter file.");
+            }
+        } else {
+            try {
+                FileReader fr=new FileReader(filename);
+                while ((ch=fr.read())!=-1)
+                    System.out.print((char)ch);
+            } catch (IOException e) {
+                System.out.println("Something went wrong with reading the counter file.");
+            }
+        }
+
+
+
         return counter;
     }
 }
