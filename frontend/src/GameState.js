6acd36d6e7f3f62aae365b1371296932f2bfa910
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-27 21:35:16 +0200

    Remove unneeded p tags.

--- frontend/src/GameState.js
+++ frontend/src/GameState.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 class GameState extends React.Component {
     render () {
         return (
-            <Typography color="textSecondary">
-                <p>Your points: {this.props.points}</p>
+            <Box>
+                <Typography color="textSecondary">
+                    Your points: {this.props.points}
+                </Typography>
                 {this.props.button}
-                <p>Your last win: {this.props.lastWin}</p>
-                <p>Clicks to next win: {this.props.clicksToNextWin}</p>
-            </Typography>
+                <Typography color="textSecondary">
+                    Your last win: {this.props.lastWin}
+                </Typography>
+                <Typography color="textSecondary">
+                    Clicks to next win: {this.props.clicksToNextWin}
+                </Typography>
+            </Box>
         )
     }
 }
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,6 +3,15 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 // A custom theme for this app
 const theme = createMuiTheme({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+    },
     palette: {
         primary: {
             main: '#556cd6',
