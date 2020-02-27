39107f18be11b0a1fa2a45c96c123d1b2aa1c435
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-27 22:01:43 +0200

    Change GameState to a react function.

--- frontend/src/GameState.js
+++ frontend/src/GameState.js
@@ -2,23 +2,19 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 
-class GameState extends React.Component {
-    render () {
-        return (
-            <Box>
-                <Typography color="textSecondary">
-                    Your points: {this.props.points}
-                </Typography>
-                {this.props.button}
-                <Typography color="textSecondary">
-                    Your last win: {this.props.lastWin}
-                </Typography>
-                <Typography color="textSecondary">
-                    Clicks to next win: {this.props.clicksToNextWin}
-                </Typography>
-            </Box>
-        )
-    }
+export default function GameState(props) {
+    return (
+        <Box>
+            <Typography color="textSecondary">
+                Your points: {props.points}
+            </Typography>
+            {props.button}
+            <Typography color="textSecondary">
+                Your last win: {props.lastWin}
+            </Typography>
+            <Typography color="textSecondary">
+                Clicks to next win: {props.clicksToNextWin}
+            </Typography>
+        </Box>
+    )
 }
-
-export default GameState
