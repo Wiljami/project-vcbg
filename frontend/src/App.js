a4b0cfa8375d23b36cbb77aa572947b4c20c7c84
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-27 21:36:26 +0200

    Remove unused import.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import GameState from './GameState'
 import GameButton from './GameButton'
