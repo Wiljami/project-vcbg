2fba72c6bd94c96cac0928561f3061796142fef6
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-27 22:00:24 +0200

    Change GameButton to a react function.
    The react function is tighter solution to what we want.

--- frontend/src/GameButton.js
+++ frontend/src/GameButton.js
@@ -1,16 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 
-class GameButton extends React.Component {
-    constructor(props) {
-        super(props)
-        this.click = props.click
-        this.text = props.text
-    }
-
-    render() {
-        return <Button onClick={this.props.click} color={this.props.color} >{this.props.text}</Button>
-    }
+export default function GameButton(props) {
+    return <Button onClick={props.click} color={props.color} >{props.text}</Button>
 }
-
-export default GameButton
