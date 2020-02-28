7aed354642aa3eae1b5c41295c5dde205a70241c
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-28 11:31:42 +0200

    Add logic to check if player is winning to the GameState.

--- frontend/src/GameState.js
+++ frontend/src/GameState.js
@@ -16,14 +16,22 @@ export default function GameState(props) {
             </Typography>
         )
     } else {
+        if (props.lastWin === 0) {
+            data.push(
+                <Typography color="textSecondary">
+                    No win on your last click.
+                </Typography>
+            )
+        } else {
+            data.push(
+                <Typography color="textSecondary">
+                    You win {props.lastWin} points!
+                </Typography>
+            )
+        }
         data.push(
             <Typography color="textSecondary">
-                Your points: {props.points}
-            </Typography>
-        )
-        data.push(
-            <Typography color="textSecondary">
-                Your last win: {props.lastWin}
+                Clicks to next win: {props.clicksToNextWin}
             </Typography>
         )
     }
