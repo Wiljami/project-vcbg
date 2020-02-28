7c7489d2828e8efced0f82a7b904555476768ec1
Viljami Pietarila <viljami.pietarila@gmail.com>
2020-02-28 11:33:47 +0200

    Revert "Add logic to check if player is winning to the GameState."
    This reverts commit 782f639cd65167a757ff7e266e710b9c508c2f47.

--- frontend/src/GameState.js
+++ frontend/src/GameState.js
@@ -16,22 +16,14 @@ export default function GameState(props) {
             </Typography>
         )
     } else {
-        if (props.lastWin === 0) {
-            data.push(
-                <Typography color="textSecondary">
-                    No win on your last click.
-                </Typography>
-            )
-        } else {
-            data.push(
-                <Typography color="textSecondary">
-                    You win {props.lastWin} points!
-                </Typography>
-            )
-        }
         data.push(
             <Typography color="textSecondary">
-                Clicks to next win: {props.clicksToNextWin}
+                Your points: {props.points}
+            </Typography>
+        )
+        data.push(
+            <Typography color="textSecondary">
+                Your last win: {props.lastWin}
             </Typography>
         )
     }
